50cef3b10e3f4df4be4ea3f7a2f488670334aeda
Swathi Kotturu <kotturuswathi@yahoo.com>
2014-12-07 12:23:18 -0800

    Added Menu

--- gen/cs175/hw5/tictactoe/R.java
+++ gen/cs175/hw5/tictactoe/R.java
@@ -28,18 +28,22 @@ public final class R {
         public static final int vazio=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080002;
+        public static final int scoresButton=0x7f080001;
+        public static final int startButton=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int main_screen=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
+        public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int scores_button=0x7f050003;
+        public static final int start_button=0x7f050002;
     }
     public static final class style {
         /** 
--- src/cs175/hw5/tictactoe/MyDb.java
+++ src/cs175/hw5/tictactoe/MyDb.java
@@ -6,7 +6,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 /**
- * A SQLiteOpenhelper to create "snake.db" and table hw4
+ * A SQLiteOpenhelper to create "tictactoe.db" and table hw5
  * 
  */
 public class MyDb extends SQLiteOpenHelper {
@@ -17,26 +17,26 @@ public class MyDb extends SQLiteOpenHelper {
 	 * @param context
 	 */
 	public MyDb(Context context) {
-		super(context, "snake.db", null, 3);
+		super(context, "tictactoe.db", null, 3);
 		// activity, database name, , version
 	}
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		// create table at first time
-		String sql = "create table if not exists hw4 (name varchar(50), speed integer, HIGH_SCORE integer, score intger)";
+		String sql = "create table if not exists hw5 (name varchar(50), speed integer, HIGH_SCORE integer, score intger)";
 		db.execSQL(sql);
 
-		Cursor rawQuery = db.rawQuery("select * from hw4", null);
+		Cursor rawQuery = db.rawQuery("select * from hw5", null);
 		if (rawQuery.getCount() == 0) {
-			db.execSQL("insert into hw4 (name, HIGH_SCORE) values('player 1', 0)");
+			db.execSQL("insert into hw5 (name, HIGH_SCORE) values('player 1', 0)");
 		}
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int arg1, int arg2) {
 		// drop and create table
-		db.execSQL("drop table if exists hw4");
+		db.execSQL("drop table if exists hw5");
 		onCreate(db);
 	}
 
